3c4087058f94677c06cc63ef8b19acf256d4f137
Arthurstuart <Arthur.stuart10@gmail.com>
2021-02-17 09:33:54 -0500

    m

--- word_frequency.py
+++ word_frequency.py
@@ -7,7 +7,27 @@ STOP_WORDS = [
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    with open(file) as praise_song:
+        text = praise_song.read()
+    word_list = text.split(" ")
+    word_list_copy = text.replace("-","")
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        for element in text:
+            if element in punctuations:
+                text = text.replace(element, "")
+        # text = text.replace("\n", " ")
+    word_list_copy 
+    word_list_copy = word_list.copy()
+    for word in word_list:
+        if word in STOP_WORDS:
+            word_list_copy.remove(word)
+    word_frequency = {}
+    for word in word_list_copy:
+        if word in word_frequency.keys():
+            word_frequency[word] += 1
+        else:
+            word_frequency[word] = 1
+    print(word_frequency)
 
 
 if __name__ == "__main__":
